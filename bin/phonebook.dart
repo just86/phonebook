d5b4dc067eb640a9dea7c896c09cf8892fed2c79
just.wt19i <just.wt19i@gmail.com>
2022-03-18 20:03:29 +0200

    18032022 2003

--- bin/phonebook.dart
+++ bin/phonebook.dart
@@ -1,5 +1,5 @@
 // import 'package:phonebook/phonebook.dart' as phonebook;
-import 'package:phonebook/phoneDB.dart' as DB;
+// import 'package:phonebook/phoneDB.dart' as db;
 import 'dart:io';
 import 'dart:math';
 import 'dart:core';
@@ -7,9 +7,9 @@ import 'dart:convert';
 
 
 void main(List<String> arguments) {
+  const pathToFile = 'lib/phoneDB.dart';
 
-  print('Hello world: ${DB.calculate()}!');
-
+  final fileDB = createFileDBSync(pathToFile);
   print('''
                       Телефонная книга города Мухосранск
   Выберите действие:
@@ -25,18 +25,62 @@ void main(List<String> arguments) {
       exit(1);
       break;
     case 1:
-      main2();
+      // bool weHaveAFile = false;
+      //
+      // final isFile = File(pathToFile).exists();
+      // isFile.then((value) => weHaveAFile = true);
+      //
+      // if (!weHaveAFile) {
+      //  myFile = File(pathToFile).create();
+
+      // } else {
+      //   print(false);
+      // }
+      // Future<File> writeLog('String pdfData') async {
+      //   final file = await _localFile;
+      //   final fileExists = await file.exists();
+      //
+      //   if(!fileExists) await file.create(); // Create your file if it does not exists.
+      //
+      //   //write to file
+      //   var sink = file.openWrite(mode: FileMode.append);
+      //   file.writeAsString("OPERATION" + pdfData.toString());
+      //   await sink.flush();
+      //   await sink.close();
+      // return file;
+
+      // }
       break;
     case 2:
-
-      break;
+     fileDB.writeAsStringSync('hi');
+     break;
   }
 }
 
-void main2() async {
-  //var file = File(Platform.script.toFilePath());
-  var file = File('');
-  print(await (file.readAsString()));
+Future<File> createFileDB(pathToFile) async {
+  // final f = File(pathToFile);
+  final file = await File(pathToFile);
+  final fileExists = await file.exists();
+
+  if (!fileExists)
+    await file.create(); // Create your file if it does not exists.
+
+  //write to file
+  // var sink = file.openWrite(mode: FileMode.append);
+  // file.writeAsString("OPERATION WRITE");
+  // await sink.flush();
+  // await sink.close();
+  return file;
+}
+
+File createFileDBSync(pathToFile) {
+  final file = File(pathToFile);
+  bool fileExists = file.existsSync();
+
+  if (!fileExists)
+    file.createSync(); // Create your file if it does not exists.
+
+  return file;
 }
 
 //чтение строки из терминала и возврат числа
